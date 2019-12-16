144ac73db581401a38d2fe46121b8df201279c3b
mugiarthur24 <catzanova24@gmail.com>
2019-12-16 20:06:24 +0700

    update laporan pegawai perskpd
    1

--- application/controllers/admin/Export_excel.php
+++ application/controllers/admin/Export_excel.php
@@ -108,6 +108,30 @@ class Export_excel extends CI_Controller {
 			redirect(base_url('index.php/login'));
 		}
 	}
+	public function ex_by_skpd($skpd){
+		if ($this->ion_auth->logged_in()) {
+			$level = array('admin','members');
+			if (!$this->ion_auth->in_group($level)) {
+				$pesan = 'Anda tidak memiliki Hak untuk Mengakses halaman ini';
+				$this->session->set_flashdata('message', $pesan );
+				redirect(base_url('index.php/admin/dashboard'));
+			}else{
+				$data['title'] = 'Pegawai Per SKPD';
+                $data['infopt'] = $this->Admin_m->info_pt(1);
+                $data['brand'] = 'asset/img/lembaga/'.$this->Admin_m->info_pt(1)->logo_pt;
+                $data['users'] = $this->ion_auth->user()->row();
+                $data['aside'] = 'nav/nav';
+                $data['page'] = 'admin/layout-perskpd-v';
+				$data['hasil'] = $this->Admin_m->data_pegawai6(20);
+				// echo "<pre>";print_r($data['hasil']);echo "<pre/>";exit();
+				$this->load->view('admin/dashboard-v', $data);
+			}
+		}else{
+			$pesan = 'Login terlebih dahulu';
+			$this->session->set_flashdata('message', $pesan );
+			redirect(base_url('index.php/login'));
+		}
+	}
 	public function export_by_pendidikan(){
 		if ($this->ion_auth->logged_in()) {
 			$level = array('admin','members');
